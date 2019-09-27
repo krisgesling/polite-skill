7c0b3e27b6b56d615dbcd548a676fac75e428bc3
Kris Gesling <krisgesling@gmail.com>
2019-09-27 15:39:29 +0930

    init commit

--- README.md
+++ README.md
@@ -1,19 +1,19 @@
-# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/robot.svg" card_color="#40DBB0" width="50" height="50" style="vertical-align:bottom"/> Polite
-Example skill showing the use of the converse() method.
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/chalkboard-teacher.svg" card_color="#4D8EDB" width="50" height="50" style="vertical-align:bottom"/> Polite
+Example Skill to show use of the converse() method.
 
 ## About
+A simple example Skill that shows how the `converse()` method can be used to take action on utterances before they reach the normal intent handling process.
 
+This Skill catches any utterance that contains a word defined in `vocab/lang-code/RudeWords.voc` and responds. Or if no rude word is detected, allows Mycroft to respond normally.
+
+For more details see the [technical documentation on converse](https://mycroft-core.readthedocs.io/en/latest/source/mycroft.html?highlight=converse#mycroft.MycroftSkill.converse).
 
 ## Examples
 * "You are stupid"
+* "Will you shut up"
 
 ## Credits
 krisgesling
 
 ## Category
-**Information**
-
-## Tags
-#Template
-#Example
-
+**Example**
--- __init__.py
+++ __init__.py
@@ -3,13 +3,37 @@ from mycroft import MycroftSkill, intent_file_handler
 
 class Polite(MycroftSkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        super().__init__()
+        # Keep track of how many impolite utterances have been made.
+        # This is only to change dialog later in the Skill.
+        self.impolite_count = 0
 
-    @intent_file_handler('polite.intent')
-    def handle_polite(self, message):
-        self.speak_dialog('polite')
+    def initialize(self):
+        """ Call make_active each time the wakeword is heard.
+            converse() can only trigger if a Skill is active. """
+        self.add_event("recognizer_loop:wakeword", self.make_active)
+
+    def converse(self, utterances, lang=None):
+        """ The converse method is able to inspect utterances before
+            they are passed to the normal intent handling service. """
+        self.log.debug("Impolite utterance detected")
+        utt = utterances[0]
+        # Check if utt contains any words in vocab/lang-code/RudeWords.voc
+        if self.voc_match(utt, "RudeWords"):
+            self.impolite_count += 1
+            self.log.debug("counter: " + str(self.impolite_count))
+            # Speak different dialog based on the state of this Skill
+            if self.impolite_count < 3:
+                self.speak_dialog("not.polite")
+            else:
+                self.speak_dialog("you.have.detention")
+            # return True to tell Mycroft that we have handled the utterance
+            return True
+        else:
+            # If no RudeWord found, return False to pass this utterance on
+            # to the normal intent handling process.
+            return False
 
 
 def create_skill():
     return Polite()
-
